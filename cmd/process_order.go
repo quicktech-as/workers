622410981eb77c0ac8a1ce601f31a2b13a1599e7
Armando <aalves.job@gmail.com>
2017-08-06 13:29:54 -0300

    Add WaitGroup to sync between goroutines and recurrent way to do query requests in database.

--- cmd/process_order.go
+++ cmd/process_order.go
@@ -2,14 +2,17 @@ package cmd
 
 import (
 	"fmt"
+	"sync"
+
 	"github.com/quicktech-as/workers/models"
 
 	"github.com/jmoiron/sqlx"
 )
 
 // ProcessOrder process a order
-func ProcessOrder(order models.Order, db *sqlx.DB) {
+func ProcessOrder(order models.Order, db *sqlx.DB, wg *sync.WaitGroup) {
 	db.MustExec("UPDATE orders SET status='close' WHERE id=?", order.ID)
 	fmt.Printf("ORDER PROCESSED : %s\n", order.UUID)
 	fmt.Println("----------------------------------------------")
+	wg.Done()
 }
--- main.go
+++ main.go
@@ -1,29 +1,34 @@
 package main
 
 import (
+	"sync"
+	"time"
+
 	"github.com/quicktech-as/workers/cmd"
 	"github.com/quicktech-as/workers/connection"
 	"github.com/quicktech-as/workers/models"
 )
 
 func main() {
-	newOrdersCh := make(chan models.Order)
-
 	db, err := connection.Get()
 	if err != nil {
 		panic(err)
 	}
-
 	defer db.Close()
 
-	for {
-		cmd.GetNewOrders(newOrdersCh, db)
+	newOrdersCh := make(chan models.Order)
+	wg := sync.WaitGroup{}
+	wg.Add(1)
 
-		go func() {
+	go func() {
+		for {
 			select {
 			case order := <-newOrdersCh:
-				go cmd.ProcessOrder(order, db)
+				go cmd.ProcessOrder(order, db, &wg)
+			case <-time.After(time.Second * 5):
+				go cmd.GetNewOrders(newOrdersCh, db, &wg)
 			}
-		}()
-	}
+		}
+	}()
+	wg.Wait()
 }
